51fedaa72ccff7d5bd4fa66dc487468a1b9d7ffc
Wael Moharram <wael.moharram@gmail.com>
2023-03-12 23:33:26 +0200

    style locale

--- resources/views/website/layouts/style.blade.php
+++ resources/views/website/layouts/style.blade.php
@@ -1,7 +1,12 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Alexandria:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="{{asset('/')}}/asset/css/bootstrap-rtl.min.css">
+@if(app()->getLocale() == 'en')
+<link rel="stylesheet" href="{{asset('/')}}/asset/css/bootstrap.min.css">
+@else
+    <link rel="stylesheet" href="{{asset('/')}}/asset/css/bootstrap-rtl.min.css">
+
+@endif
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
