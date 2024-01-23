6455e4b608cc19dbc242ec4f137e1175ce012b3d
Wael Moharram <wael.moharram@gmail.com>
2024-01-23 14:33:57 +0200

    New dashboard login page

--- resources/views/dashboard/auth/login.blade.php
+++ resources/views/dashboard/auth/login.blade.php
@@ -1,148 +1,117 @@
 <!DOCTYPE html>
-<html class="loading" lang="en" data-textdirection="ltr">
-<!-- BEGIN: Head-->
+<html lang="en">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
-    <meta name="author" content="{{env('APP_NAME')}}">
-    <title>Login {{env('APP_NAME')}}</title>
-    <link rel="apple-touch-icon" href="{{asset('assets/dashboard2/resources')}}/app-assets/images/ico/apple-icon-120.png">
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/dashboard2/resources')}}/app-assets/images/ico/favicon.ico">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Purple Admin</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{asset('assets/mabeet-dashboard')}}/assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="{{asset('assets/mabeet-dashboard')}}/assets/vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Alexandria:wght@400;500;600;700;800&family=Tajawal:wght@200;300;400;500;700;800;900&display=swap" rel="stylesheet">
-    <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard2/resources')}}/app-assets/vendors/css/vendors.min.css">
-    <!-- END: Vendor CSS-->
-
-    <!-- BEGIN: Theme CSS-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard2/resources')}}/app-assets/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard2/resources')}}/app-assets/css/bootstrap-extended.css">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard2/resources')}}/app-assets/css/colors.css">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard2/resources')}}/app-assets/css/components.css">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard2/resources')}}/app-assets/css/themes/dark-layout.css">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard2/resources')}}/app-assets/css/themes/bordered-layout.css">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard2/resources')}}/app-assets/css/themes/semi-dark-layout.css">
-
-    <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard2/resources')}}/app-assets/css/core/menu/menu-types/vertical-menu.css">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard2/resources')}}/app-assets/css/plugins/forms/form-validation.css">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard2/resources')}}/app-assets/css/pages/page-auth.css">
-    <!-- END: Page CSS-->
-
-    <!-- BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/dashboard2/resources')}}/assets/css/style.css">
-    <!-- END: Custom CSS-->
-
-    <style>
-        body{
-            font-family:"Tajawal" ;
-        }
-    </style>
-
+    <link href="https://fonts.googleapis.com/css2?family=Alexandria:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('assets/mabeet-dashboard')}}/assets/css/style.css">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="{{asset('assets/mabeet-dashboard')}}/assets/images/favicon.ico" />
 </head>
-<!-- END: Head-->
 
-<!-- BEGIN: Body-->
+<style>
+    .login-body
+    {
+        background: #cfeacb;
+    }
+    .login-form .card
+    {
+        background: none !important;
+    }
 
-<body class="vertical-layout vertical-menu-modern blank-page navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="blank-page">
-<!-- BEGIN: Content-->
-<div class="app-content content ">
-    <div class="content-overlay"></div>
-    <div class="header-navbar-shadow"></div>
-    <div class="content-wrapper">
-        <div class="content-header row">
-        </div>
-        <div class="content-body">
-            <div class="auth-wrapper auth-v2">
-                <div class="auth-inner row m-0">
-                    <!-- Brand logo--><a class="brand-logo" href="javascript:void(0);">
-                        <h2 class="brand-text text-primary ml-1">{{env('APP_NAME')}}</h2>
-                    </a>
-                    <!-- /Brand logo-->
-                    <!-- Left Text-->
-                    <div class="d-none d-lg-flex col-lg-8 align-items-center p-5">
-                        <div class="w-100 d-lg-flex align-items-center justify-content-center px-5"><img class="img-fluid" src="{{asset('assets/dashboard2/resources')}}/app-assets/images/pages/login-v2.svg" alt="Login V2" /></div>
-                    </div>
-                    <!-- /Left Text-->
-                    <!-- Login-->
-                    <div class="d-flex col-lg-4 align-items-center auth-bg px-2 p-lg-5">
-                        <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
-                            <h2 class="card-title font-weight-bold mb-1">{{__('Login')}}</h2>
-                            <p class="card-text mb-2">{{__('Welcome back, please login to your account.')}}</p>
-                            <form class="auth-login-form mt-2" action="{{ url('dashboard/login') }}" method="POST">
-                                @csrf
-                                <div class="form-group">
-                                    <label class="form-label" for="email">{{__('Email')}}</label>
-                                    <input class="form-control" id="email" type="text" name="email" placeholder="john@example.com" aria-describedby="email" autofocus="" tabindex="1" />
-                                    {{input_error($errors,'email')}}
-                                </div>
-                                <div class="form-group">
-                                    <div class="d-flex justify-content-between">
-                                        <label for="password">{{__('Password')}}</label>
-{{--                                        <a href="page-auth-forgot-password-v2.html"><small>Forgot Password?</small></a>--}}
-                                    </div>
-                                    <div class="input-group input-group-merge form-password-toggle">
-                                        <input class="form-control form-control-merge" id="password" type="password" name="password" placeholder="············" aria-describedby="login-password" tabindex="2" />
-                                        <div class="input-group-append"><span class="input-group-text cursor-pointer"><i data-feather="eye"></i></span></div>
-                                        {{input_error($errors,'password')}}
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="custom-control custom-checkbox">
-                                        <input class="custom-control-input" name="remember" id="remember" type="checkbox" tabindex="3" />
-                                        <label class="custom-control-label" for="remember"> {{__('Remember me')}}</label>
-                                    </div>
-                                </div>
-                                <button class="btn btn-primary btn-block" tabindex="4">{{__('Login')}}</button>
-                            </form>
-{{--                            <p class="text-center mt-2"><span>New on our platform?</span><a href="page-auth-register-v2.html"><span>&nbsp;Create an account</span></a></p>--}}
-{{--                            <div class="divider my-2">--}}
-{{--                                <div class="divider-text">or</div>--}}
-{{--                            </div>--}}
-{{--                            <div class="auth-footer-btn d-flex justify-content-center"><a class="btn btn-facebook" href="javascript:void(0)"><i data-feather="facebook"></i></a><a class="btn btn-twitter white" href="javascript:void(0)"><i data-feather="twitter"></i></a><a class="btn btn-google" href="javascript:void(0)"><i data-feather="mail"></i></a><a class="btn btn-github" href="javascript:void(0)"><i data-feather="github"></i></a></div>--}}
-{{--                        --}}
-                        </div>
-                    </div>
-                    <!-- /Login-->
-                </div>
+    .login-form .card button
+    {
+        background: #006e2a;
+        color: white
+    }
+
+</style>
+
+<body class="login-body">
+<div class="login">
+    <div class="container pt-3">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <img class="img-fluid" src="{{asset('assets/mabeet-dashboard')}}/assets/images/logo.png">
+            </div>
+            <div class="col-md-6">
+{{--                <a class="text-left d-block" href="#">English</a>--}}
             </div>
         </div>
     </div>
 </div>
-<!-- END: Content-->
 
 
-<!-- BEGIN: Vendor JS-->
-<script src="{{asset('assets/dashboard2/resources')}}/app-assets/vendors/js/vendors.min.js"></script>
-<!-- BEGIN Vendor JS-->
+<div class="login-form container">
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <h2 class="text-center text-dark mt-5">دخول المسئولين</h2>
+            <div class="text-center mb-2 text-dark">اكتب بيانات الدخول للوحة التحكم</div>
+            <div class="card my-2">
 
-<!-- BEGIN: Page Vendor JS-->
-<script src="{{asset('assets/dashboard2/resources')}}/app-assets/vendors/js/forms/validation/jquery.validate.min.js"></script>
-<!-- END: Page Vendor JS-->
+                <form class="p-lg-5" action="{{ url('dashboard/login') }}" method="POST">
 
-<!-- BEGIN: Theme JS-->
-<script src="{{asset('assets/dashboard2/resources')}}/app-assets/js/core/app-menu.js"></script>
-<script src="{{asset('assets/dashboard2/resources')}}/app-assets/js/core/app.js"></script>
-<!-- END: Theme JS-->
+                    @csrf
+                    <div class="mb-3">
+                        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email"
+                               placeholder="البريد الالكترونى">
+                        {{input_error($errors,'email')}}
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control" id="password" placeholder="كلمة المرور" name="password">
+                        {{input_error($errors,'password')}}
+                    </div>
+                    <div class="form-group">
+                        <div class="custom-control custom-checkbox">
+                            <input class="custom-control-input" name="remember" id="remember" type="checkbox" tabindex="3" />
+                            <label class="custom-control-label" for="remember"> {{__('Remember me')}}</label>
+                        </div>
+                    </div>
+                    <div class="text-center"><button type="submit" class="btn btn-color px-5 mb-5 w-100">الدخول للوحة التحكم</button></div>
+{{--                    <div id="emailHelp" class="form-text text-center mb-5 text-dark">Not--}}
+{{--                        Registered? <a href="#" class="text-dark fw-bold"> Create an--}}
+{{--                            Account</a>--}}
+{{--                    </div>--}}
+                </form>
+            </div>
 
-<!-- BEGIN: Page JS-->
-<script src="{{asset('assets/dashboard2/resources')}}/app-assets/js/scripts/pages/page-auth-login.js"></script>
-<!-- END: Page JS-->
+        </div>
+    </div>
+</div>
 
-<script>
-    $(window).on('load', function() {
-        if (feather) {
-            feather.replace({
-                width: 14,
-                height: 14
-            });
-        }
-    })
-</script>
+
+
+<script src="{{asset('assets/mabeet-dashboard')}}/assets/vendors/js/vendor.bundle.base.js"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<script src="{{asset('assets/mabeet-dashboard')}}/assets/vendors/chart.js/Chart.min.js"></script>
+<script src="{{asset('assets/mabeet-dashboard')}}/assets/js/jquery.cookie.js" type="text/javascript"></script>
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src="{{asset('assets/mabeet-dashboard')}}/assets/js/off-canvas.js"></script>
+<script src="{{asset('assets/mabeet-dashboard')}}/assets/js/hoverable-collapse.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.min.js" integrity="sha512-cWEytOR8S4v/Sd3G5P1Yb7NbYgF1YAUzlg1/XpDuouZVo3FEiMXbeWh4zewcYu/sXYQR5PgYLRbhf18X/0vpRg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="{{asset('assets/mabeet-dashboard')}}/assets/js/misc.js"></script>
+<!-- endinject -->
+<!-- Custom js for this page -->
+<script src="{{asset('assets/mabeet-dashboard')}}/assets/js/dashboard.js"></script>
+<script src="{{asset('assets/mabeet-dashboard')}}/assets/js/todolist.js"></script>
+<!-- End custom js for this page -->
 </body>
-<!-- END: Body-->
 
 </html>
