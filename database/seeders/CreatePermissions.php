faec7f03521454d02f915c8ef1ca0416ee27f62a
Wael Moharram <wael.moharram@gmail.com>
2024-02-20 15:53:41 +0200

    seeds

--- database/seeders/CreatePermissions.php
+++ database/seeders/CreatePermissions.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 
-class createPermissions extends Seeder
+class CreatePermissions extends Seeder
 {
     /**
      * Run the database seeds.
